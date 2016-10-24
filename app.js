7edb65dc611ab3553868f21b2ac00212d9f6a2d4
uroojgithub <uroojonline.khan@gmail.com>
2016-10-24 19:17:16 +0530

    files ADDED

--- app.js
+++ app.js
@@ -3,15 +3,7 @@
   angular.module("myApp", [])
 
   .controller("myController", function($scope){
-    $scope.name="";
-    $scope.count= 0;
-    $scope.no = function(){
-      var no = 0;
-      $scope.count = no;
+    
     };
-    function calNo(string){
-
-    }
   });
-
 })();
